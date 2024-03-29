c57b9dbbe0143aacbe1271fe002767d91513802e
KhufuLintalou <148902267+KhufuLintalou@users.noreply.github.com>
2024-03-29 15:28:59 +0700

    Deleted the test codes, oops.

--- janken.js
+++ janken.js
@@ -40,6 +40,3 @@ function playRound(playerSelection, computerSelection) {
         }
     }
 }
-const playerSelection = "scissors";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
