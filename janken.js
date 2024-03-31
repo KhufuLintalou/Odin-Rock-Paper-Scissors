a2901a6ad8e61a6af34948ef4a997301ac460540
KhufuLintalou <148902267+KhufuLintalou@users.noreply.github.com>
2024-03-31 22:24:51 +0700

    Added some changes and the last needed function.

--- janken.js
+++ janken.js
@@ -16,27 +16,53 @@ function playRound(playerSelection, computerSelection) {
         if (computer === "rock") {
             return "Tie!";
         } else if (computer === "paper") {
-            return "You lose! Paper beats Rock";
+            return "Computer win!";
         } else {
-            return "You win! Rock beats Scissor";
+            return "You win!";
         }
     }
     if (player === "paper") {
         if (computer === "paper") {
             return "Tie!";
         } else if (computer === "rock") {
-            return "You win! Paper beats Rock";
+            return "You win!";
         } else {
-            return "You lose! Scissors beats Paper";
+            return "Computer win!";
         }
     }
     if (player === "scissors") {
         if (computer === "scissors") {
             return "Tie!";
         } else if (computer === "paper") {
-            return "You win! Scissors beats Paper";
+            return "You win!";
         } else {
-            return "You lose! Rock beats Scissors";
+            return "Computer win!";
         }
+    }
+}
+
+function playGame() {
+    let playerScore = 0;
+    let computerScore = 0;
+    for (i = 0; i < 5; i++) {
+        switch ((playRound(prompt("What's your choice?"), getComputerChoice()))) {
+        case "You win!":
+            playerScore++;
+            console.log(`You won this round! Your Score:${playerScore}`);
+            break;
+        case "Computer win!":
+            computerScore++;
+            console.log(`Computer won this round! Computer's Score:${computerScore}`);
+            break;
+        case "Tie!":
+            console.log("This round is a tie!");
+        }
+    }  
+    if (playerScore === computerScore) {
+        console.log("Game end in a Tie!");
+    } else if (playerScore < computerScore) {
+        console.log("Computer is the Winner!") 
+    } else {
+        console.log("You are the Winner!")
     }
 }
