5f3ef705e09e5c3df21fc03937d011bffb5a2fb8
KhufuLintalou <148902267+KhufuLintalou@users.noreply.github.com>
2024-03-31 23:08:03 +0700

    Added comments to explains all of the code.

--- janken.js
+++ janken.js
@@ -1,18 +1,19 @@
+// Function to get the computer choice for the game.
 function getComputerChoice() {
-    let randomNumber = Math.floor(Math.random() * 3);
+    let randomNumber = Math.floor(Math.random() * 3);   // This gives a random number.
     if (randomNumber == 0) {
         return "Rock";
-    } else if (randomNumber == 1) {
+    } else if (randomNumber == 1) {   // Assigned the random numbers to an appropriate string.
         return "Paper";
     } else {
         return "Scissors";
     };
 }
-
+// Function to play a single round.
 function playRound(playerSelection, computerSelection) {
     let player = playerSelection.toLowerCase();
     let computer = computerSelection.toLowerCase();
-    if (player === "rock") {
+    if (player === "rock") {   // If player choice was rock.
         if (computer === "rock") {
             return "Tie!";
         } else if (computer === "paper") {
@@ -21,7 +22,7 @@ function playRound(playerSelection, computerSelection) {
             return "You win!";
         }
     }
-    if (player === "paper") {
+    if (player === "paper") {   // If player choice was paper.
         if (computer === "paper") {
             return "Tie!";
         } else if (computer === "rock") {
@@ -30,7 +31,7 @@ function playRound(playerSelection, computerSelection) {
             return "Computer win!";
         }
     }
-    if (player === "scissors") {
+    if (player === "scissors") {   // If player choice was scissors.
         if (computer === "scissors") {
             return "Tie!";
         } else if (computer === "paper") {
@@ -40,25 +41,25 @@ function playRound(playerSelection, computerSelection) {
         }
     }
 }
-
+// Function that play the game for five rounds, keep the scores and report the winner.
 function playGame() {
-    let playerScore = 0;
+    let playerScore = 0;   // Created the variable for scoring.
     let computerScore = 0;
-    for (i = 0; i < 5; i++) {
-        switch ((playRound(prompt("What's your choice?"), getComputerChoice()))) {
-        case "You win!":
+    for (i = 0; i < 5; i++) {   // This is to loop the function for 5 times.
+        switch ((playRound(prompt("What's your choice?"), getComputerChoice()))) {   // playRound function with prompt for player input and getComputerChoice for computer input.
+        case "You win!":   // In case the player won, add a score to it and report the results of the round.
             playerScore++;
             console.log(`You won this round! Your Score:${playerScore}`);
             break;
-        case "Computer win!":
+        case "Computer win!":   // In case the computer won, add a score to it and report the results.
             computerScore++;
             console.log(`Computer won this round! Computer's Score:${computerScore}`);
             break;
-        case "Tie!":
+        case "Tie!":   // This is when they are tied.
             console.log("This round is a tie!");
         }
     }  
-    if (playerScore === computerScore) {
+    if (playerScore === computerScore) {   // This is for comparing the scores and announce the final result of all five rounds.
         console.log("Game end in a Tie!");
     } else if (playerScore < computerScore) {
         console.log("Computer is the Winner!") 
