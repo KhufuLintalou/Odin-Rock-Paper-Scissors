0c028d3917a88beabfbe25507688bca1ef31a999
KhufuLintalou <148902267+KhufuLintalou@users.noreply.github.com>
2024-05-14 20:27:04 +0700

    Added some code to make the buttons do something.

--- janken.js
+++ janken.js
@@ -17,28 +17,45 @@ function playRound(playerSelection, computerSelection) {
         if (computer === "rock") {
             return "Tie!";
         } else if (computer === "paper") {
-            return "Computer win!";
+            return "You Lose! Computer chooses Paper."
         } else {
-            return "You win!";
+            return "You Win! Computer chooses Scissors.";
         }
     }
     if (player === "paper") {   
         if (computer === "paper") {
             return "Tie!";
         } else if (computer === "rock") {
-            return "You win!";
+            return "You Win! Computer chooses Rock.";
         } else {
-            return "Computer win!";
+            return "You Lose! Computer chooses Scissors.";
         }
     }
     if (player === "scissors") {   
         if (computer === "scissors") {
             return "Tie!";
         } else if (computer === "paper") {
-            return "You win!";
+            return "You win! Computer chooses Paper.";
         } else {
-            return "Computer win!";
+            return "You lose! Computer chooses Rock.";
         }
     }
 }
+
+let result = document.querySelector(".result");
+
+let choiceRock = document.querySelector("#rock");
+choiceRock.addEventListener("click", function() {
+    result.textContent = (playRound("Rock", getComputerChoice()));
+});
+
+let choicePaper = document.querySelector("#paper");
+choicePaper.addEventListener("click", function() {
+    result.textContent = (playRound("Paper", getComputerChoice()));
+})
+
+let choiceScissors = document.querySelector("#scissors");
+choiceScissors.addEventListener("click", function() {
+    result.textContent = (playRound("Scissors", getComputerChoice()));
+})
 
