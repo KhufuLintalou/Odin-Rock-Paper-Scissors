a896cbb7210ac18cceb61510428a5aa36c41c199
KhufuLintalou <148902267+KhufuLintalou@users.noreply.github.com>
2024-05-14 19:49:23 +0700

    Remove the function that play 5 rounds.

--- janken.js
+++ janken.js
@@ -41,29 +41,4 @@ function playRound(playerSelection, computerSelection) {
         }
     }
 }
-// Function that play the game for five rounds, keep the scores and report the winner.
-function playGame() {
-    let playerScore = 0;   
-    let computerScore = 0;
-    for (i = 0; i < 5; i++) {   
-        switch ((playRound(prompt("What's your choice?"), getComputerChoice()))) { 
-        case "You win!":   
-            playerScore++;
-            console.log(`You won this round! Your Score:${playerScore}`);
-            break;
-        case "Computer win!":   
-            computerScore++;
-            console.log(`Computer won this round! Computer's Score:${computerScore}`);
-            break;
-        case "Tie!":   
-            console.log("This round is a tie!");
-        }
-    }  
-    if (playerScore === computerScore) {
-        console.log("Game end in a Tie!");
-    } else if (playerScore < computerScore) {
-        console.log("Computer is the Winner!") 
-    } else {
-        console.log("You are the Winner!")
-    }
-}
+
